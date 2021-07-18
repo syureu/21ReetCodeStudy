0f850f73bfc28c885026715a616e4d08d3aa7def
syureu <36031420+syureu@users.noreply.github.com>
2021-07-18 13:52:27 +0900

    Time: 252 ms (18.52%), Space: 36.9 MB (20.88%) - LeetHub

---
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.kt
@@ -0,0 +1,26 @@
+import kotlin.math.max
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    var depth = 0
+    var maxDepth = 0
+    fun maxDepth(root: TreeNode?): Int {
+        if (root != null) {
+            depth++
+            maxDepth = max(depth, maxDepth)
+            if (root.left != null) maxDepth(root.left)
+            if (root.right != null) maxDepth(root.right)
+            depth--
+        }
+        return maxDepth
+    }
+}
