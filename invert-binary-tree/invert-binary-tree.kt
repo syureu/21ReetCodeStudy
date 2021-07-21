f635ffbef8fe78f10a54cc6c5010cc18e0dd1920
syureu <36031420+syureu@users.noreply.github.com>
2021-07-21 23:42:59 +0900

    Time: 144 ms (55.56%), Space: 33.5 MB (88.24%) - LeetHub

--- invert-binary-tree/invert-binary-tree.kt
+++ invert-binary-tree/invert-binary-tree.kt
@@ -18,3 +18,19 @@ class Solution {
         return root
     }
 }
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+fun main() {
+    val root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.left!!.left = TreeNode(1)
+    root.left!!.right = TreeNode(3)
+    root.right = TreeNode(7)
+    root.right!!.left = TreeNode(6)
+    root.right!!.right = TreeNode(9)
+    Solution().invertTree(root)
+}
