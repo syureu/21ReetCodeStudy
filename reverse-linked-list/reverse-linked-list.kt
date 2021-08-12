64e92b20a343ad922878ba854b4cf8f51b2ad5ef
syureu <36031420+syureu@users.noreply.github.com>
2021-08-12 20:43:38 +0900

    Time: 152 ms (94.00%), Space: 36.1 MB (33.03%) - LeetHub

--- reverse-linked-list/reverse-linked-list.kt
+++ reverse-linked-list/reverse-linked-list.kt
@@ -11,13 +11,30 @@ class Solution {
     fun reverseList(head: ListNode?): ListNode? {
         var prev: ListNode? = null
         var cur = head
-        var next = head?.next
+        var next: ListNode?
         while (cur != null) {
+            next = cur.next
             cur.next = prev
             prev = cur
             cur = next
-            next = next?.next
         }
         return prev
     }
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun main() {
+    val root: ListNode? = ListNode(1)
+    var node = root
+    node!!.next = ListNode(2)
+    node = node.next
+    node!!.next = ListNode(3)
+    node = node.next
+    node!!.next = ListNode(4)
+    node = node.next
+    node!!.next = ListNode(5)
+    val ans = Solution().reverseList(root)
 }
