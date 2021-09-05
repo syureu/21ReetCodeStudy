bdf0f3896f2a011f9c03caf648ec5ff7adb336db
syureu <36031420+syureu@users.noreply.github.com>
2021-09-05 22:14:20 +0900

    Time: 300 ms (77.73%), Space: 44.4 MB (79.55%) - LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val ans = IntArray(nums.size)
+        ans[0] = 1
+        for (i in 1 until nums.size) ans[i] = ans[i - 1] * nums[i - 1]
+        var tmp = 1
+        for (i in nums.size - 1 downTo 0) {
+            ans[i] *= tmp
+            tmp *= nums[i]
+        }
+        return ans
+    }
+}
